17bc10ea7a975ab5e04618523bc34176839586cb
PikuzaMarie <pikuza.mari@gmail.com>
2024-11-06 20:51:29 +0300

    style: style layout of todo component on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import TodoList from "./components/TodoList";
+import { Box } from "@mui/material";
 
 function App() {
 	return (
-		<>
+		<Box
+			sx={{
+				height: "100vh",
+				width: "100vw",
+				backgroundColor: "#e5e5ed",
+				paddingTop: "10%",
+			}}>
 			<TodoList />
-		</>
+		</Box>
 	);
 }
 
